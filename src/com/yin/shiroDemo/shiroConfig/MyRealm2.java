580cd56ad78b03d6bc5d8ea8fb042d0389d9b2a5
yinxiuchaun <1362486131@qq.com>
2019-05-21 15:42:48 +0800

    单realm认证，多realm认证。单元测试。

---
+++ src/com/yin/shiroDemo/shiroConfig/MyRealm2.java
@@ -0,0 +1,43 @@
+package yin.shiroDemo.shiroConfig;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * @BelongsProject: shiroDemo
+ * @BelongsPackage: yin.shiroDemo.shiroConfig
+ * @Author: yinxiucahun
+ * @CreateTime: 2019-05-21 15:08
+ * @Description: 单Realm配置
+ */
+public class MyRealm2 implements Realm {
+    @Override
+    public String getName() {
+        return "myRealm1";
+    }
+
+    @Override
+    public boolean supports(AuthenticationToken token) {
+        //仅支持UsernamePasswordToken
+        // instanceof 判断其左边对象是否为其右边类的实例，返回boolean类型的数据
+        return token instanceof UsernamePasswordToken;
+    }
+
+    @Override
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        //得到用户名
+        String username = (String)token.getPrincipal();
+        //得到密码
+        String password = new String((char[])token.getCredentials());
+        if(!"wang".equals(username)){
+            //用户名错误
+            throw new UnknownAccountException();
+        }
+        if(!"123".equals(password)){
+            //密码错误
+            throw new IncorrectCredentialsException();
+        }
+        //如果身份认证验证成功，返回一个 AuthenticationInfo 实现；
+        return new SimpleAuthenticationInfo(username,password,getName());
+    }
+}
--- src/test/testHelloWorld.java
+++ src/test/testHelloWorld.java
@@ -18,7 +18,7 @@ import org.junit.Test;
 public class testHelloWorld {
 
     @Test
-    public void testHelloWorld() {
+    public void testHelloWorld1() {
         //获取 SecurityManager 工厂，此处使用 Ini 配置文件初始化 SecurityManager
         Factory<SecurityManager> factory =
                 new IniSecurityManagerFactory("classpath:shiro.ini");
@@ -40,6 +40,56 @@ public class testHelloWorld {
         Assert.assertEquals(true,subject.isAuthenticated());
         //退出
         subject.logout();
+
     }
 
+    @Test
+    public void testHelloWorld2() {
+        //获取 SecurityManager 工厂，此处使用 Ini 配置文件初始化 SecurityManager
+        Factory<SecurityManager> factory =
+                new IniSecurityManagerFactory("classpath:shiro-realm.ini");
+        //得到 SecurityManager 实例 并绑定给 SecurityUtils（全局设置，设置一次即可）
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+        //得到 Subject 及创建用户名/密码身份验证 Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang","123");
+
+        try{
+            //登录（验证身份）
+            subject.login(token);
+        }catch (AuthenticationException e){
+            //身份验证失败
+            System.out.println("错误");
+        }
+        //断言 断言用户已经登陆
+        Assert.assertEquals(true,subject.isAuthenticated());
+        //退出
+        subject.logout();
+    }
+
+    @Test
+    public void testHelloWorld3() {
+        //获取 SecurityManager 工厂，此处使用 Ini 配置文件初始化 SecurityManager
+        Factory<SecurityManager> factory =
+                new IniSecurityManagerFactory("classpath:shiro-multi-realm.ini");
+        //得到 SecurityManager 实例 并绑定给 SecurityUtils（全局设置，设置一次即可）
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+        //得到 Subject 及创建用户名/密码身份验证 Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("wang","123");
+
+        try{
+            //登录（验证身份）
+            subject.login(token);
+        }catch (AuthenticationException e){
+            //身份验证失败
+            System.out.println("错误");
+        }
+        //断言 断言用户已经登陆
+        Assert.assertEquals(true,subject.isAuthenticated());
+        //退出
+        subject.logout();
+    }
 }
