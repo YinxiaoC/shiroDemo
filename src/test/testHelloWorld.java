b779166f17830db6ac8db7f8d64958cb8a5bfc29
yinxiuchaun <1362486131@qq.com>
2019-05-21 15:04:30 +0800

    测试demo，读取ini文件，断言登录

---
+++ src/test/testHelloWorld.java
@@ -0,0 +1,45 @@
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @BelongsProject: shiroDemo
+ * @BelongsPackage: PACKAGE_NAME
+ * @Author: yinxiucahun
+ * @CreateTime: 2019-05-21 14:44
+ * @Description: 测试用例
+ */
+public class testHelloWorld {
+
+    @Test
+    public void testHelloWorld() {
+        //获取 SecurityManager 工厂，此处使用 Ini 配置文件初始化 SecurityManager
+        Factory<SecurityManager> factory =
+                new IniSecurityManagerFactory("classpath:shiro.ini");
+        //得到 SecurityManager 实例 并绑定给 SecurityUtils（全局设置，设置一次即可）
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+        //得到 Subject 及创建用户名/密码身份验证 Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang","123");
+
+        try{
+            //登录（验证身份）
+            subject.login(token);
+        }catch (AuthenticationException e){
+            //身份验证失败
+            System.out.println("错误");
+        }
+        //断言 断言用户已经登陆
+        Assert.assertEquals(true,subject.isAuthenticated());
+        //退出
+        subject.logout();
+    }
+
+}
